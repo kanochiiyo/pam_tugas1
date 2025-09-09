267f1c1aab25d7dc4c57f2ee2316f45830c05afd
kanochiiyo <124230084@student.upnyk.ac.id>
2025-09-09 17:06:11 +0700

    fix gradle

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:login_app/pages/auth_screen.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions().currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(home: AuthScreen()));
 }
